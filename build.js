e5e90f883922ff878eb56075db4700deb2e3884b
Ewan Howell <48070995+ewanhowell5195@users.noreply.github.com>
2025-07-26 20:39:09 +0100

    Fix broken max-width

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ import CleanCSS from "clean-css"
 import { minify } from "terser"
 import fs from "node:fs"
 
-const version = "1.2.1"
+const version = "1.2.2"
 
 fs.mkdirSync("dist", { recursive: true })
 
